c90a25c13dfec04b257f57562e75d286cc51260d
David Caycedo <69812733+DcDevRep@users.noreply.github.com>
2024-04-03 18:04:58 -0500

    build: :zap: Updating the project to JS, creating the connection with the database, and the first start route.
    Updating the project to JS, creating the connection with the database, and the first start route.
    
    Updating the project to JS, creating the connection with the database, and the first start route.

--- server/src/app.js
+++ server/src/app.js
@@ -1,25 +1,38 @@
-import 'dotenv/config'
-import express from 'express'
-import logger from 'morgan'
-import cookieParser from 'cookie-parser'
-import appRouter from './routers/index.js'
+import express from "express";
 
-const app = express()
-const PORT = process.env.PORT
+import logger from "morgan";
+import cookieParser from "cookie-parser";
+import favicon from 'serve-favicon';
+import appRouter from "./routers/index.js";
+import { uploader } from "./utils/multer.js";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { connectDB } from "./database/db.js";
+import { config } from "dotenv";
 
-app.use(express.json())
-app.use(express.urlencoded({ extends: true }))
-app.use(logger('dev'))
-app.use(cookieParser())
+// TODO: Implement route creation and CRUD API logic
 
-app.post('/file', uploader.single('myFile'), (req, res) => {
-    res.send('Image uploaded')
-  })
+const app = express();
+const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const root = join(__dirname, "/assets/ico", "favicon.ico");
+config()
 
-app.use(appRouter)
+app.use(express.json());
+app.use(express.urlencoded({ extends: true }));
+app.use(logger("dev"));
+app.use(cookieParser());
+
+app.use(favicon(root));
+app.post("/file", uploader.single("myFile"), (req, res) => {
+  res.send("Image uploaded");
+});
+
+app.use(appRouter);
 
 app.listen(PORT, (err) => {
-    if (err) console.log(err)
-    console.log(`Server listening on port ${PORT}`)
-})
-
+  if (err) console.log(err);
+  console.log(`✅ Server 🆗 is running 💯 on http://localhost:${PORT}`);
+  connectDB();
+});
