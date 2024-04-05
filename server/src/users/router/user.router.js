da54480e4dc01332a8be6d7e88fc94cfa9639cd6
agustin garcia maggi <agustingmaggi@gmail.com>
2024-04-05 13:13:14 +0200

    adding bcrypt and jwt to register and login

--- server/src/users/router/user.router.js
+++ server/src/users/router/user.router.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import { deleteUser, editUser, getUserById, getUsers, postUser } from "../controllers/user.controller.js";
-
+// import { deleteUser, editUser, getUserById, getUsers, postUser } from "../controllers/user.controller.js";
+import { register, login, profile } from "../../controllers/SessionController.js"
+import { validateJWT } from "../../middlewares/jwtExtractor.js";
 
 export const userRoutes = Router();
 
-userRoutes.post('/create', postUser);
-userRoutes.get('/', getUsers);
-userRoutes.get('/:id', getUserById);
-userRoutes.patch('/edit/:id', editUser);
-userRoutes.delete('/delete/:id', deleteUser);
+// userRoutes.post('/create', postUser);
+// userRoutes.get('/', getUsers);
+// userRoutes.get('/:id', getUserById);
+// userRoutes.patch('/edit/:id', editUser);
+// userRoutes.delete('/delete/:id', deleteUser);
+
+userRoutes.get('/profile', validateJWT, profile)
+userRoutes.post('/register', register)
+userRoutes.post('/login', login)
