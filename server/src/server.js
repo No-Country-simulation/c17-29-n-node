f8111f44350c0bdd0dabf81cd830264fe7f86a81
David Caycedo <69812733+DcDevRep@users.noreply.github.com>
2024-04-10 22:13:08 -0500

    fix: :zap: fixing folder structure and deploy for vercel
    fixing folder structure and deploy for vercel
    
    fixing folder structure and deploy for vercel

--- server/src/server.js
+++ server/src/server.js
@@ -9,17 +9,18 @@ import { fileURLToPath } from "url";
 import { uploader } from "./shared/multer/multer.js";
 config();
 
-const app = express();
 const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const root = join(__dirname, "/assets/ico/favicon.ico");
 
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extends: true }));
 app.use(logger("dev"));
 app.use("/assets", express.static(join(__dirname, "assets")));
 
-app.use(favicon(join(__dirname, "/assets/ico/favicon.ico")));
+app.use(favicon(root));
 
 app.post("/file", uploader.single("myFile"), (req, res) => {
   res.send("Image uploaded");
@@ -29,6 +30,8 @@ app.use(appRouter);
 
 app.listen(PORT, (err) => {
   if (err) console.log(err);
-  console.log(`✅ Server 🆗 is running 💯 on http://localhost:${PORT}/api/docs`);
+  console.log(
+    `✅ Server 🆗 is running 💯 on http://localhost:${PORT}/api/docs`
+  );
   connectDB();
 });
--- server/src/shared/Multer/multer.js
+++ server/src/shared/Multer/multer.js
@@ -7,7 +7,7 @@ const __dirname = dirname(__filename);
 
 const storage = multer.diskStorage({
   destination: function (req, file, callback) {
-    callback(null, join(__dirname,'/public/image'));
+    callback(null, join(__dirname,'/assets/img'));
   },
   filename: function (req, file, callback) {
     callback(null, `${Date.now()}-${file.originalname}`);
