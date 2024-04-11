74c56fb0d6abcbf07e96ed35aa7a47fa777b30a4
David Caycedo <69812733+DcDevRep@users.noreply.github.com>
2024-04-10 22:31:49 -0500

    fix: :zap: despoly for vercel
    deploy for vercel
    
    deploy for vercel

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,18 @@
 import express from "express";
 import logger from "morgan";
 import favicon from "serve-favicon";
-import appRouter from "./routers/index.js";
 import { config } from "dotenv";
 import { dirname, join } from "path";
-import { connectDB } from "./shared/database/db.js";
 import { fileURLToPath } from "url";
+
+import appRouter from "./routers/index.js";
+import { connectDB } from "./shared/database/db.js";
 import { uploader } from "./shared/multer/multer.js";
+
 config();
 
 const PORT = process.env.PORT || 3000;
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const root = join(__dirname, "/assets/ico/favicon.ico");
@@ -19,17 +22,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extends: true }));
 app.use(logger("dev"));
 app.use("/assets", express.static(join(__dirname, "assets")));
-
 app.use(favicon(root));
 
-app.post("/file", uploader.single("myFile"), (req, res) => {
+app.post("/api/file", uploader.single("myFile"), (req, res) => {
   res.send("Image uploaded");
 });
 
 app.use(appRouter);
 
 app.listen(PORT, (err) => {
-  if (err) console.log(err);
+  if (err) console.error(err);
   console.log(
     `✅ Server 🆗 is running 💯 on http://localhost:${PORT}/api/docs`
   );
