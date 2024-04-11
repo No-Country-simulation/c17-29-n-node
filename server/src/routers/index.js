93353c7256aa65c74dada56a3066362866f04bd4
rogelioromo <rogelioromo25@gmail.com>
2024-04-11 16:29:51 -0700

    commit to add new CRUD for cars

--- server/src/routers/index.js
+++ server/src/routers/index.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-
-import { controller, middleware } from "../shared/sawgger/swagger.config.js";
+import { controller, middleware } from "../shared/swagger/swagger.config.js";
 import { invalidRouter } from "../apis/Invalid/router/invalid.router.js";
 import { startRouter } from "../apis/start/router/start.router.js";
 import { authRoutes } from "../apis/auth/router/auth.router.js";
 import { userRoutes } from "../apis/users/router/user.router.js";
+import { carRouter } from "../apis/cars/router/car.router.js";
 
 const router = Router();
 
@@ -12,6 +12,7 @@ router.use("/api/docs", middleware, controller);
 router.use("/api/start", startRouter);
 router.use("/api/users", userRoutes);
 router.use("/api/auth", authRoutes);
+router.use("/api/cars", carRouter);
 router.use("*", invalidRouter);
 
 export default router;
