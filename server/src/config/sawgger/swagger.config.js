e55a7e52c87db5902df770b72e6a303b41c4f0b0
David Caycedo <69812733+DcDevRep@users.noreply.github.com>
2024-04-11 23:28:04 -0500

    feat: :zap: to display in vercel

--- server/src/config/sawgger/swagger.config.js
+++ server/src/config/sawgger/swagger.config.js
@@ -53,7 +53,7 @@ const swaggerConfig = {
     consumes: ["application/json"],
     produces: ["application/json"],
   },
-  apis: ["./src/**/doc/*.doc.js", "./src/**/**/doc/*.doc.js"],
+  apis: ["src/**/*.doc.js"],
 };
 
 const swaggerOptions = {
