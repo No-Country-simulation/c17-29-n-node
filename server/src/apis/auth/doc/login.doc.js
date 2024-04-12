a06e8860b32ed21b5c6cb31ec4a9bc89102dd352
David Caycedo <69812733+DcDevRep@users.noreply.github.com>
2024-04-12 01:06:53 -0500

    fix: :zap: to display in vercel

--- server/src/apis/auth/doc/login.doc.js
+++ server/src/apis/auth/doc/login.doc.js
@@ -16,38 +16,38 @@
  *          example: 1234
  * paths:
  *  /auth/login:
- *   post:
- *    tags:
+ *    post:
+ *     tags:
  *      - Auth Route
- *    summary: login of the app.
- *    description: login of the app.
- *    operationId: login
- *    requestBody:
- *      description: login of the app.
- *      required: true
- *      content:
- *        application/json:
- *          schema:
- *            $ref: '#/components/schemas/Login'
- *    responses:
- *      200:
- *        description: Successful operation
- *        content:
+ *     summary: login of the app.
+ *     description: login of the app.
+ *     operationId: login
+ *     requestBody:
+ *       description: login of the app.
+ *       required: true
+ *       content:
  *         application/json:
- *          schema:
- *            $ref: '#/components/schemas/Login'
- *      404:
- *        description: Not Found
- *        content:
- *         application/json:
- *          schema:
- *            $ref: '#/components/schemas/NotFound'
- *      500:
- *        description: Internal Server Error
- *        content:
- *         application/json:
- *          schema:
- *            $ref: '#/components/schemas/NotServer'
+ *           schema:
+ *             $ref: '#/components/schemas/Login'
+ *     responses:
+ *       200:
+ *         description: Successful operation
+ *         content:
+ *          application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Login'
+ *       404:
+ *         description: Not Found
+ *         content:
+ *          application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/NotFound'
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *          application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/NotServer'
  *    security:
  *      - api_auth:
  *         - write:api
